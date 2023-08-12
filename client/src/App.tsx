d04df30a3f9a0514ae480ddb5692dd378375628f
wagmi7 <94379406+wagmi7@users.noreply.github.com>
2023-08-12 03:46:47 +0000

    aa with base implementation

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -12,33 +12,36 @@ import { AssetPriceContext, useAssetPrice } from '../src/context/AssetPriceConte
 import { EthBalance } from '../src/pages/EthBalance';
 import { NFTList } from "./components/Nft/DisplayNFT";
 import OmniGovernDao from "./pages/Dao";
+import { AccountAbstractionProvider } from "./context/accountAbstractionContext";
 
 const App = () => {
   const assetPrice = useAssetPrice();
 
   return (
     <div>
-      <AssetPriceContext.Provider value={assetPrice}>
-        <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route
-            path="/*"
-            element={
-              <AppShell padding="md" navbar={<Navbar />} header={<Header />}>
-                <Routes>
-                  <Route path="/dashboard" element={<Home />} />
-                  <Route path="/profile" element={<Profile />} />
-                  <Route path="/campaign" element={<CreateCampaign />} />
-                  <Route path="/campaign-details/:id" element={<CampaignDetails />} />
-                  <Route path="/analyse" element={<EthBalance />} />
-                  <Route path="/allnfts" element={<NFTList />} />
-                  <Route path="/dao" element={<OmniGovernDao />} />
-                </Routes>
-              </AppShell>
-            }
-          />
-        </Routes>
-      </AssetPriceContext.Provider>
+      <AccountAbstractionProvider>
+        <AssetPriceContext.Provider value={assetPrice}>
+          <Routes>
+            <Route path="/" element={<Landing />} />
+            <Route
+              path="/*"
+              element={
+                <AppShell padding="md" navbar={<Navbar />} header={<Header />}>
+                  <Routes>
+                    <Route path="/dashboard" element={<Home />} />
+                    <Route path="/profile" element={<Profile />} />
+                    <Route path="/campaign" element={<CreateCampaign />} />
+                    <Route path="/campaign-details/:id" element={<CampaignDetails />} />
+                    <Route path="/analyse" element={<EthBalance />} />
+                    <Route path="/allnfts" element={<NFTList />} />
+                    <Route path="/dao" element={<OmniGovernDao />} />
+                  </Routes>
+                </AppShell>
+              }
+            />
+          </Routes>
+        </AssetPriceContext.Provider>
+      </AccountAbstractionProvider>
     </div>
   );
 };
